ce39d11fb95a888ac735c68e378ed0d442a1cb8c
karpeev <serg.386p@gmail.com>
2018-09-07 21:47:25 +0300

    refactor game object and add game life cycle

--- src/js/game-state.js
+++ src/js/game-state.js
@@ -1,25 +1,26 @@
-const gameState =
-{
-    collectedBugs:0,
-    uncollectedBugs:0,
-    gameTime:'',
-    coffeeCups:0,
-    setGraphics:()=>{},
-    emitMovement:(direction)=>{},
-    isWin:false,
-    isLose:false
-}
 
 export default class GameState{
     constructor(){
-        this.collectedBugs=0,
-        this.uncollectedBugs=0,
-        this.gameTime='',
-        this.coffeeCups=0,
-        this.isWin=false,
-        this.isLose=false
+        this.collectedBugs=0;
+        this.uncollectedBugs=0;
+        this.gameTime='';
+        this.coffeeCups=0;
+        this.isWin=false;
+        this.isLose=false;
+        this.graphics=null;
     }
 
-    setGraphics(){}
+    setGraphics(graphis){
+        this.graphics = graphics;
+    }
     emitMovement(direction){}
+
+    gameLifeCycle = new GameLifeCycle()
+
+}
+
+class GameLifeCycle{
+    constructor(){}
+    render(){}
+    calculate(){}
 }
