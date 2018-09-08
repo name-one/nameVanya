058b45a626520ec8a533556786a62d2d55b72c9f
DenisKamensky <denis.kamensky@nameone.io>
2018-09-08 12:26:26 +0300

    eventEmmiter

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 import loadGraphic from './js/graphic-loader';
 import GameState from './js/game-state'
 import loadImage from './js/image-loader';
+import GameEventEmitter from './js/game-event-emitter';
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const gameEventEmitter = new GameEventEmitter();
 loadImage('assets/canvas-bg.png').then(img => {
     const gameState = new GameState(ctx, canvas, img);
     loadGraphic(ctx).then(graphic => {
         console.log(graphic)
         gameState.setGraphics(graphic);
-        gameState.loadLevel('/assets/level.txt')
+        gameState.loadLevel('/assets/level.txt');
+        //gameEventEmitter.subscribeOnEvents(()
     });
 })
